657769214cc16c9b607ab6ef81ef0101c0146ff5
arijitmandal10 <arijitmandal999@gmail.com>
2023-09-07 04:25:04 +0530

    final

--- app/components/Card2/Card2.jsx
+++ app/components/Card2/Card2.jsx
@@ -1,23 +1,64 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useRef } from 'react';
+import { Card, Typography } from '@mui/material';
 import styles from './card2.module.css';
 import Image from 'next/image';
 import img1 from '../../assets/img1.png';
-import { Card, Typography } from '@mui/material';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Card2 = () => {
-	return (
-		<Card className={styles.card2}>
-			<div className={styles.left}>
-				<h4 style={{ margin: '30px' }}>Built out of frustration</h4>
+	const cardRef = useRef(null);
 
+	useEffect(() => {
+		const card = cardRef.current;
+
+		// Initialize the animation timeline
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: card,
+				start: 'top 80%',
+				end: 'bottom 20%',
+				scrub: true, // Smooth scrubbing effect
+			},
+		});
+
+		// Define the animations
+		tl.fromTo(
+			card.querySelector('.left'),
+			{ x: -50, opacity: 0, scale: 0.5 },
+			{ x: 0, opacity: 1, scale: 1, duration: 2, ease: 'elastic.out(1, 0.)' },
+		);
+
+		tl.fromTo(
+			card.querySelector('.right'),
+			{ x: 50, opacity: 0, scale: 0.5 },
+			{ x: 0, opacity: 1, scale: 1, duration: 2, ease: 'elastic.out(1, 0.4)' },
+			'-=0.5', // Delay the right-side animation slightly
+		);
+
+		// You can add more animations within this timeline as needed
+
+		return () => {
+			// Kill the ScrollTrigger and animations when the component unmounts
+			tl.kill();
+			ScrollTrigger.getById('card2Trigger').kill();
+		};
+	}, []);
+
+	return (
+		<Card className={styles.card2} ref={cardRef}>
+			<div className={`${styles.left} left`}>
+				<h4 style={{ margin: '30px' }}>Built out of frustration</h4>
 				<p style={{ margin: '30px' }}>Meet the ahead app</p>
 				<Image src={img1} width={550} height={350} />
 			</div>
-
-			<div className={styles.right}>
+			<div className={`${styles.right} right`}>
 				<Typography variant='h6' component='h6'>
-					A personalized pocket coach that provides bite- sized, science-driven tools to boost emotional intelligence. <br /> Think of
-					it as a pocket cheerleader towards a better, more fulfilling.
+					A personalized pocket coach that provides bite-sized, science-driven tools to boost emotional intelligence.
+					<br /> Think of it as a pocket cheerleader towards a better, more fulfilling.
 				</Typography>
 			</div>
 		</Card>
--- app/components/VerticalStepper/VerticalStepper.jsx
+++ app/components/VerticalStepper/VerticalStepper.jsx
@@ -1,12 +1,30 @@
+'use client';
+import React, { useState } from 'react';
 import styles from './verticalstepper.module.css';
 import icon2 from '../../assets/icon2.png';
 import Image from 'next/image';
-import React from 'react';
 import { Stepper, Step, StepLabel, Typography, Card } from '@mui/material';
 
-const steps = ['Step 1', 'Step 2', 'Step 3'];
+const steps = [
+	{
+		a: 'Its not as easy as 1-2-3.',
+		b: 'The journey of change may be long, but our sessions are quick. We get to the point and tell you what you want to know (and nothing else). ',
+	},
+
+	{ a: 'Old habits are hard to break.', b: 'And bad behaviors die hard. Fortunately, we give you great, science- backed techniques to use. ' },
+	{
+		a: 'You and your motivation dont have a long-term relationship.',
+		b: 'Luckily, we can proactively prepare you for the marathon, not just the race. Effective, memorable exerciseswill help you stick to your goals.',
+	},
+	{
+		a: "Books just don't offer practical",
+		b: "Remember when you learned to ride a bike just by reading? Yeah, we don't either. We help you take concrete smallsteps towards your goals. Learning just a bit, then immediately springing into action.",
+	},
+];
 
 const VerticalStepper = () => {
+	const [activeStep, setActiveStep] = useState(-1);
+
 	return (
 		<div className={styles.steps}>
 			<div className={styles.top}>
@@ -18,37 +36,19 @@ const VerticalStepper = () => {
 
 			<div style={{ width: '80%', margin: ' 85px auto' }}>
 				<Stepper orientation='vertical'>
-					<Step>
-						<StepLabel>
-							<b>It's not as easy as 1-2-3.</b>
-							<br /> The journey of change may be long, but our sessions are quick. We get to the point and tell you what you
-							want to know (and nothing else).
-						</StepLabel>
-					</Step>
-					<Step>
-						<StepLabel>
-							<b>Old habits are hard to break.</b>
-							<br />
-							And bad behaviors die hard. Fortunately, we give you great, science- backed techniques to use. 
-						</StepLabel>
-					</Step>
-					<Step>
-						<StepLabel>
-							<b>You and your motivation don't have a long-term relationship.</b>
-							<br />
-							Luckily, we can proactively prepare you for the marathon, not just the race. Effective, memorable exerciseswill
-							help you stick to your goals.
-						</StepLabel>
-					</Step>
-					<Step>
-						<StepLabel>
-							{' '}
-							<b>Books just don't offer practical</b>
-							<br />
-							Remember when you learned to ride a bike just by reading? Yeah, we don't either. We help you take concrete small
-							steps towards your goals. Learning just a bit, then immediately springing into action.
-						</StepLabel>
-					</Step>
+					{steps.map((label, index) => (
+						<Step
+							key={label}
+							onMouseEnter={() => setActiveStep(index)}
+							onMouseLeave={() => setActiveStep(-1)}
+							active={index === activeStep}
+						>
+							<StepLabel>
+								<b>{label.a}</b>
+								<br /> {label.b}.
+							</StepLabel>
+						</Step>
+					))}
 				</Stepper>
 			</div>
 
@@ -61,7 +61,7 @@ const VerticalStepper = () => {
 					<p>Not having your own emotions under control might be holding you back.</p>
 				</div>
 				<div>
-					<p>Additionally, not understanding those of others stops you from being parent, friend you can be.</p>
+					<p>Additionally, not understanding those of others stops you from being the parent, friend you can be.</p>
 				</div>
 			</div>
 		</div>
